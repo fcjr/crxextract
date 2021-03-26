b8a06df59a0d29bb130607a0e95a03b463e585e7
Frank Chiarulli Jr <frank@frankchiarulli.com>
2021-03-26 18:15:14 -0400

    adjust styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,45 @@
 <script type='ts'>
-	import Dropzone from "svelte-file-dropzone";
-	import FileSaver from "file-saver";
+	import Dropzone from "svelte-file-dropzone"
+	import FileSaver from "file-saver"
 
 	function uint32FromUint8Array(data: Uint8Array) {
-		let dataview = new DataView(data.buffer);
-		return dataview.getInt32(0, true);
+		let dataview = new DataView(data.buffer)
+		return dataview.getInt32(0, true)
 	}
 
 	function getCrxHeader(data: Uint8Array) {
-		const decoder = new TextDecoder();
+		const decoder = new TextDecoder()
 		const magic = decoder.decode(data.slice(0, 4))
 		const version = uint32FromUint8Array(data.slice(4, 8))
 		const length = uint32FromUint8Array(data.slice(8, 12))
-		const header = { magic, version, length }
-		return header 
+		return { magic, version, length } 
 	}
 
 	function processCrx(name: string, data: Uint8Array) {
-		const header = getCrxHeader(data);
-		const blob = new Blob([data.slice(header.length + 12)], {type: "text/plain;charset=utf-8"});
-		FileSaver.saveAs(blob, name + '.zip');
+		const header = getCrxHeader(data)
+		const blob = new Blob([data.slice(header.length + 12)], {type: "text/plain;charset=utf-8"})
+		FileSaver.saveAs(blob, name + '.zip')
   	}
 
 	function processXpi(name: string, data: ArrayBuffer) {
-		const blob = new Blob([data], {type: "text/plain;charset=utf-8"});
-		FileSaver.saveAs(blob, name + '.zip');
+		const blob = new Blob([data], {type: "text/plain;charset=utf-8"})
+		FileSaver.saveAs(blob, name + '.zip')
   	}
 
 	function handleFilesSelect(e: any) {
-    	const files = e.detail.acceptedFiles;
+    	const files = e.detail.acceptedFiles
     	for (let i = 0; i < files.length; i++) {
-			const name = files[i].name.slice(0, -4);
-			const type = files[i].name.slice(-3);
-      		const reader = new FileReader();
+			const name = files[i].name.slice(0, -4)
+			const type = files[i].name.slice(-3)
+      		const reader = new FileReader()
       		reader.onload = () => {
-        		const buf = reader.result as ArrayBuffer;
+        		const buf = reader.result as ArrayBuffer
 				if (type === 'xpi') {
-					processXpi(name, buf);
+					processXpi(name, buf)
 				} else {
-					processCrx(name, new Uint8Array(buf));
+					processCrx(name, new Uint8Array(buf))
 				}
-			};
+			}
 			reader.readAsArrayBuffer(files[i])
     	}
   	}
@@ -52,6 +51,7 @@
 		on:drop={handleFilesSelect}
 		multiple={false}
 		accept=".crx,.xpi"
+		containerStyles='height: 100%; justify-content: center;'
 	>
 	<p>Drop a .crx or .xpi here to extract</p>
 	</Dropzone>
@@ -60,10 +60,11 @@
 
 <style>
 	main {
+		width: 65%;
+		height: 65%;
 		text-align: center;
 		padding: 1em;
 		margin: 0 auto;
-		width: 80%;
 	}
 
 	h1 {
