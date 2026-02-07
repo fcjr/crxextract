266ba00bb64352ba90f22c7aac785d7558d013f7
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2026-02-06 20:06:15 -0500

    bump chrome version

--- src/utils/getCrxUrl.ts
+++ src/utils/getCrxUrl.ts
@@ -1,4 +1,4 @@
 export default function getCrxUrl(extensionId: string) {
-	const baseUrl = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&acceptformat=crx3&x=id%3D__EXTENSION_ID__%26installsource%3Dondemand%26uc'
+	const baseUrl = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=130.0&acceptformat=crx3&x=id%3D__EXTENSION_ID__%26installsource%3Dondemand%26uc'
 	return baseUrl.replace('__EXTENSION_ID__', extensionId)
 }
